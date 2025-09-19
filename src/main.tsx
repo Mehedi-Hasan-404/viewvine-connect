aafd561e4a98a94721d133282892399b89329c50
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-09-19 18:29:56 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
-import { AdminProvider } from "./context/AdminContext.tsx"; // Add this import
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <AdminProvider>
-      <App />
-    </AdminProvider>
+    <App />
   </AuthProvider>
 );
